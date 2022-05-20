78eaa319e71dfe65810794cac9b4ca25e5855e63
c0rradini <g.siqueira@edu.unipar.br>
2022-05-20 17:59:44 -0300

    Adiciona funções para visualização, edição, delete de Produtos.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('products.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         Product::create([
             'name' => $request->name,
             'price_cost' => $request->price_cost,
@@ -20,5 +22,50 @@ class ProductsController extends Controller
         ]);
 
         return "Produto criado com Sucesso!";
+
+    }
+
+    public function show($id)
+    {
+
+        $product = Product::findOrFail($id);
+        return view('products.show', ['product' => $product]);
+
+    }
+
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.edit', ['product' => $product]);
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $product = Product::findOrFail($id);
+
+        $product->update([
+            'name' => $request->name,
+            'price_cost' => $request->price_cost,
+            'price_sale' => $request->price_sale,
+            'qtde' => $request->qtde,
+        ]);
+
+        return "Editado com Sucesso!";
+    }
+
+    public function delete($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.delete', ['product' => $product]);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return "Produto excluido com Sucesso!";
     }
 }
