439a38922fdad8b8fc518f752bea900aaadc67cc
c0rradini <g.siqueira@edu.unipar.br>
2022-05-20 14:11:33 -0300

    Criado migrate para o Produtos.

---
+++ database/migrations/2022_05_20_142811_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price_cost', 19,2);
+            $table->decimal('price_sale', 19, 2);
+            $table->integer('qtde');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
