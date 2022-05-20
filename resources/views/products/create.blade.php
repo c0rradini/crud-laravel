6106a0deda59bdd05ed032b70ff5ab4bef40746b
c0rradini <g.siqueira@edu.unipar.br>
2022-05-20 14:21:06 -0300

    Adiciona View para cadastro de novo Produto.

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de Produto</title>
+</head>
+<body>
+    <form action="{{route('register_product')}}" method="POST">
+        @csrf
+        <label for="">Nome</label>
+        <br>
+        <input type="text" name="name">
+        <br>
+        <label for="">Preço de custo</label>
+        <br>
+        <input type="text" name="price_cost">
+        <br>
+        <label for="">Preço de Venda</label>
+        <br>
+        <input type="text" name="price_sale">
+        <br><label for="">Quantidade</label>
+        <br>
+        <input type="text" name="qtde">
+        <br>
+        <button>Salvar</button>
+
+
+    </form>
+</body>
+</html>
